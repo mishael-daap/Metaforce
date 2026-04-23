9863ea6d1cb0fc522d305e805aebc9438507cce9
Mishael <themishaeldaap@gmail.com>
2026-04-23 13:22:00 +0100

    removed the frontend folder

--- frontend/.next/dev/static/chunks/[project]_pages__error_07xvfw~._.js
+++
@@ -1,5 +0,0 @@
-(globalThis["TURBOPACK_CHUNK_LISTS"] || (globalThis["TURBOPACK_CHUNK_LISTS"] = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: ["static/chunks/0y.w_next_dist_compiled_097r3x.._.js","static/chunks/0y.w_next_dist_shared_lib_011w89-._.js","static/chunks/0y.w_next_dist_client_0vf1g.c._.js","static/chunks/[root-of-the-server]__0i~1mw0._.js","static/chunks/[next]_entry_page-loader_ts_06u3~va._.js","static/chunks/0p2h_react-dom_0vsf22y._.js","static/chunks/node_modules__pnpm_13qx_gy._.js","static/chunks/[root-of-the-server]__01mw43t._.js"],
-    source: "entry"
-});
