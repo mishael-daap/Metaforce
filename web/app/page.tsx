5cdf14f3404a0df3a5e9f148a6b0792f9ed8fee7
Mishael <themishaeldaap@gmail.com>
2026-06-25 12:24:58 +0100

    fixed mobile nav and sheet issues

--- web/app/page.tsx
+++ web/app/page.tsx
@@ -1,11 +1,21 @@
 import { Metadata } from "next";
-import { auth } from "./auth"
+import { auth } from "./auth";
+import Navbar from "@/components/landing-page/Navbar";
+import MaxWidthWrapper from "@/components/landing-page/max-width-wrapper";
 
 export const metadata: Metadata = {
   title: "Home",
 };
 
-export default async function Page(){
-  const session = await auth()
-  return <div>{session?.user?.name} or wtf</div>
+export default async function Page() {
+  const session = await auth();
+  return (
+    <div className="overflow-x-hidden scrollbar-hide size-full">
+       {/* Hero Section */}
+
+            <MaxWidthWrapper>
+              <Navbar />
+            </MaxWidthWrapper>
+    </div>
+  );
 }
